fb7a2ed5b88a111d13210b95315233515af57d3d
Amir Azad <amir.azad78@gmail.com>
2024-09-15 18:44:57 +0330

    add book service

--- reposirotry/book_repository.go
+++ reposirotry/book_repository.go
@@ -6,7 +6,7 @@ type BookRepository interface {
 	GetAll() (*[]models.Book, error)
 	GetById(int64) (*models.Book, error)
 	GetByName(string) (*models.Book, error)
-	Create(book *models.Book) error
-	Update(book *models.Book) error
+	Create(*models.Book) error
+	Update(*models.Book) error
 	Delete(int64) error
 }
---
+++ service/book_service.go
@@ -0,0 +1,40 @@
+package service
+
+import (
+	"github.com/amirazad1/gin-crud/models"
+	"github.com/amirazad1/gin-crud/reposirotry"
+)
+
+type BookService struct {
+	repo reposirotry.BookRepository
+}
+
+func NewBookService(rp reposirotry.BookRepository) *BookService {
+	return &BookService{
+		repo: rp,
+	}
+}
+
+func (s *BookService) GetAll() (*[]models.Book, error) {
+	return s.repo.GetAll()
+}
+
+func (s *BookService) GetById(id int64) (*models.Book, error) {
+	return s.repo.GetById(id)
+}
+
+func (s *BookService) GetByName(name string) (*models.Book, error) {
+	return s.repo.GetByName(name)
+}
+
+func (s *BookService) Create(book *models.Book) error {
+	return s.repo.Create(book)
+}
+
+func (s *BookService) Update(book *models.Book) error {
+	return s.repo.Update(book)
+}
+
+func (s *BookService) Delete(id int64) error {
+	return s.repo.Delete(id)
+}
